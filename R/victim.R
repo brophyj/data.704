8c68384f5c3fa745686e6e244d8a7811e5ac5eaa
brophyj <james.brophy@mcgill.ca>
2022-07-05 17:08:12 -0400

    typo corrected

--- R/victim.R
+++ R/victim.R
@@ -4,7 +4,7 @@
 #' The responses by race, for those who identified themselves as black or as white, were collected.
 #' @format A tibble with 243 rows and 10 variables:
 #' \describe{
-#'   \item{resp}{Number of victims personally known to the respondent
+#'   \item{resp}{Number of victims personally known to the respondent}
 #'   \item{race}{Responses by race, for those who identified themselves as black or as white}
 #' }
 #' @source \url{https://data.library.virginia.edu/getting-started-with-negative-binomial-regression-modeling/}

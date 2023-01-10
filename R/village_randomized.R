9770c8b4dcb36445abd7dc9e7276d2d342e5bc6f
brophyj <james.brophy@mcgill.ca>
2023-01-10 18:00:31 -0500

    typo corrected

--- R/village_randomized.R
+++ R/village_randomized.R
@@ -13,7 +13,7 @@
 #'   \item{program}{Randomized intervention}
 #'   \item{post_income}{Income after the intervention}
 #'   \item{sex_num}{Male = 1, female = 0}
-#'   \item{program_num}{0 = no promgram, 1 = Program}
+#'   \item{program_num}{0 = No program, 1 = Program}
 #' }
 #' @source \url{https://evalf20.classes.andrewheiss.com/example/rcts/}
 "village_randomized"

1543684f9af787944a7220324a893a717637d388
B. Rich <mail@benjaminrich.net>
2023-01-11 11:07:01 -0500

    Add list of datasets to package documentation

--- R/data.704-package.R
+++ R/data.704-package.R
@@ -1,3 +1,15 @@
+#' data.704: Data for EBOH 704
+#'
+#' Supplies data for 704 labs and lectures.
+#'
+#' List of datasets included in this package:
+#' - [Challeng]: Challenger dataset of O-rings failure
+#' - [cannabis]: High school cannabis use
+#' - [chic]: Data of Chicago weather
+#' - [diabetes]: Diabetes dataset from a cohort study of Pima Indians
+#' - [trop]: Data of troponin values
+#' - [victim]: Victim dataset from Agresti, A. (2002). Categorical Data Analysis. p. 561
+#' - [village_randomized]: A simulated randomized trial dataset
 #' @keywords internal
 "_PACKAGE"
 
